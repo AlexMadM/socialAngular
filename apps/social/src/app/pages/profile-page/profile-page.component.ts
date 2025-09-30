6b6358466476bdb69b30623ab6548ae1f33d9b70
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-09-29 21:39:38 -0700

    fix profile page

--- apps/social/src/app/pages/profile-page/profile-page.component.ts
+++ apps/social/src/app/pages/profile-page/profile-page.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject, Inject } from '@angular/core';
 import { ProfileService } from '@apps/profile';
-import { ActivatedRoute } from '@angular/router';
-import {  switchMap } from 'rxjs';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { subscribeOn, switchMap } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 import { ProfileHeaderComponent } from '../../common-ui/profile-header.component';
+import { ImgUrlPipe } from '@apps/common-ui';
 
 
 
 @Component({
   selector: 'app-profile-page',
-  imports: [AsyncPipe, ProfileHeaderComponent],
+  imports: [AsyncPipe, ProfileHeaderComponent, RouterLink, ImgUrlPipe],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
@@ -18,13 +19,17 @@ export class ProfilePageComponent {
   profileService = inject(ProfileService);
   route = inject(ActivatedRoute);
   me$ = toObservable(this.profileService.me);
+
   profile$ = this.route.params.pipe(
     switchMap(({ id }) => {
       if (id === 'me') return this.me$;
       return this.profileService.getAccount(id);
     })
   );
+  subscribers$ = this.profileService.getSubscribersShortList();
 }
+
+
 
 
 
