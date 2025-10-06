05604cbde5b4ca9698f526842b8c84060f9f125e
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-10-05 19:32:24 -0700

    creating filter profile

--- apps/social/src/app/pages/search-page/search-page.component.ts
+++ apps/social/src/app/pages/search-page/search-page.component.ts
@@ -1,20 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { ProfileCard } from '../../common-ui/profile-card/profile-card';
 import { ProfileService } from '@apps/profile';
+import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
 
 @Component({
   selector: 'app-search-page',
-  imports: [ProfileCard],
+  imports: [ProfileCard, ProfileFiltersComponent],
   standalone: true,
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
 export class SearchPageComponent {
-
-  private profileService = inject(ProfileService);
-  profiles:any=[]
-
-  constructor() { this.profileService.getTestAccounts().subscribe(profiles => this.profiles = profiles);
-  }
-
+  profileService = inject(ProfileService);
+  profiles = this.profileService.filteredProfiles;
 }
