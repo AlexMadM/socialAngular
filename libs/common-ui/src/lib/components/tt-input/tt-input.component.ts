58afb8a2e82c4c5548e06eaf0de78f581c9d8fdb
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-10-08 19:20:24 -0700

    input component

---
+++ libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
@@ -0,0 +1,49 @@
+import { Component, forwardRef, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'tt-input',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './tt-input.component.html',
+  styleUrl: './tt-input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => TtInputComponent)
+    }
+  ]
+})
+export class TtInputComponent implements ControlValueAccessor {
+  type = input<'text' | 'password'>('text')
+  placeholder = input<string>()
+
+  disabled = signal<boolean>(false);
+
+  onChange: any
+  onTouched: any
+
+  value: string | null = null
+
+  writeValue(val: string | null) {
+    this.value = val
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled)
+  }
+
+  onModelChange(val: string | null): void {
+    this.onChange(val)
+  }
+}
