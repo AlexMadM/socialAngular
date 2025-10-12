26210d54a9c9ebc97ffc9ff461086aca7b7f3f27
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-10-12 11:01:19 -0700

    tt- input

--- libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
+++ libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
@@ -1,6 +1,15 @@
 import { Component, forwardRef, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+export type Callback<D = void, R = void> = (data?: D) => R;
+type ControlValue = string | null;
+type InputValue = Extract<ControlValue, string>;
 
 @Component({
   selector: 'tt-input',
@@ -12,38 +21,37 @@ import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModu
     {
       provide: NG_VALUE_ACCESSOR,
       multi: true,
-      useExisting: forwardRef(() => TtInputComponent)
-    }
-  ]
+      useExisting: forwardRef(() => TtInputComponent),
+    },
+  ],
 })
 export class TtInputComponent implements ControlValueAccessor {
-  type = input<'text' | 'password'>('text')
-  placeholder = input<string>()
-
+  type = input<'text' | 'password'>('text');
+  placeholder = input<string>();
+  private onChange?: Callback<InputValue>;
+  private onTouched?: Callback;
   disabled = signal<boolean>(false);
 
-  onChange: any
-  onTouched: any
-
-  value: string | null = null
+  value: string | null = null;
 
   writeValue(val: string | null) {
-    this.value = val
+    this.value = val;
   }
 
   registerOnChange(fn: any): void {
-    this.onChange = fn
+    this.onChange = fn;
   }
 
   registerOnTouched(fn: any): void {
-    this.onTouched = fn
+    this.onTouched = fn;
   }
 
   setDisabledState(isDisabled: boolean): void {
-    this.disabled.set(isDisabled)
+    this.disabled.set(isDisabled);
   }
 
   onModelChange(val: string | null): void {
-    this.onChange(val)
+    // @ts-ignore
+    this.onChange(val);
   }
 }
